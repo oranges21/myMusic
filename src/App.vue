810e177e53446a5e3d09a61b4ddbe6ee374fbb26
oranges21 <oranges21@163.com>
2024-07-10 22:05:51 +0800

    第一次搭建项目中...

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>111</div>
+  <el-button type="primary">Primary</el-button>
+</template>
+
+<style scoped></style>
+<script setup></script>
